c263a57c1ee05e70d58bcdde0cecd721650def71
fatx64 <191420072@student.binadarma.ac.id>
2023-10-16 08:55:40 +0700

    adding login with google function in LoginActivity (button login with google)

--- app/src/main/java/com/example/ekohort_android/auth/RegisterActivity.kt
+++ app/src/main/java/com/example/ekohort_android/auth/RegisterActivity.kt
@@ -2,11 +2,26 @@ package com.example.ekohort_android.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.ekohort_android.R
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+    }
+
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    fun registerUser (email: String, password: String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                task -> if (task.isSuccessful){
+                    val user = auth.currentUser
+            }else{
+                Log.w("Registration","createUserWithEmail:failure" , task.exception)
+            }
+            }
     }
 }
---
+++ app/src/main/java/com/example/ekohort_android/home_page/HomeActivity.kt
@@ -0,0 +1,76 @@
+package com.example.ekohort_android.home_page
+
+import android.annotation.SuppressLint
+import android.content.ContentProviderClient
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
+import com.example.ekohort_android.R
+import com.example.ekohort_android.auth.LoginActivity
+import com.example.ekohort_android.databinding.ActivityHomeBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var googleSigningClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var binding: ActivityHomeBinding
+
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            logoutButton.setOnClickListener {
+                signOut()
+            }
+        }
+
+        auth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("550281026161-ld1lg5ajcd8pjq1t863ue13p11tdrep1.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSigningClient = GoogleSignIn.getClient(this, gso)
+
+
+        googleSigningClient
+
+        val textView = findViewById<TextView>(R.id.name)
+
+        val user = auth.currentUser
+        val auth = Firebase.auth
+
+        if (user != null){
+            val userName = user.displayName
+            textView.text = "Welcome" + userName
+        } else{
+            //do nothing
+        }
+    }
+
+
+    private fun signOut(){
+        auth.signOut()
+
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+
+}
