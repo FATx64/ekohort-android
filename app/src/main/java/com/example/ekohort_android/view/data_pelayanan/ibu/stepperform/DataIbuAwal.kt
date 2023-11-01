649625cea3cc464d2558d1198ef6a857bf18cfdc
fatx64 <191420072@student.binadarma.ac.id>
2023-11-01 11:06:01 +0700

    add calendar on activity_data_ibu_awal.xml and DataIbuAwal.kt

--- app/src/main/java/com/example/ekohort_android/view/data_pelayanan/ibu/stepperform/DataIbuAwal.kt
+++ app/src/main/java/com/example/ekohort_android/view/data_pelayanan/ibu/stepperform/DataIbuAwal.kt
@@ -1,12 +1,22 @@
 package com.example.ekohort_android.view.data_pelayanan.ibu.stepperform
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.example.ekohort_android.R
 import com.example.ekohort_android.databinding.ActivityDataIbuAwalBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class DataIbuAwal : AppCompatActivity() {
+
+    private val calendar = Calendar.getInstance()
+
+    private var dateOfBirth: Long = System.currentTimeMillis()
+
 
     private lateinit var binding: ActivityDataIbuAwalBinding
 
@@ -16,6 +26,32 @@ class DataIbuAwal : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.apply {
+            binding.edtDoB.setOnClickListener {
+                showDatePickerDialog()
+            }
+        }
+
 
     }
+
+    private fun showDatePickerDialog(){
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{
+            view, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            val formatDate = dateFormat.format(calendar.time)
+            binding.edtDoB.setText(formatDate)
+        }, year, month, day)
+        datePickerDialog.show()
+    }
+
+
 }
