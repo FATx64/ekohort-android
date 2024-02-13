26a8c6c1e766c56e0ba262deb2c2dc00760d1cf5
ziro <palembani@gmail.com>
2024-02-13 10:57:25 +0700

    fix: Crashes

--- app/src/main/java/com/example/ekohort_android/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/ekohort_android/presentation/main/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.example.ekohort_android.presentation.main
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.example.ekohort_android.databinding.ActivityMainBinding
-import com.example.ekohort_android.presentation.base.BaseActivity
 import com.example.ekohort_android.presentation.onboarding.OnboardingScreen1
 
-class MainActivity : BaseActivity<ActivityMainBinding>() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = if (savedInstanceState == null) installSplashScreen() else null
 
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
 
         val startTime = System.currentTimeMillis()
         splashScreen?.setKeepOnScreenCondition {
