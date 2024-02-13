d43ea68c53d270ce192b6faf341ba19a1ec96e72
ziro <palembani@gmail.com>
2024-02-13 11:19:00 +0700

    chore: Add Koin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,9 @@ dependencies {
     implementation(platform("io.insert-koin:koin-bom:3.5.3"))
     implementation("io.insert-koin:koin-android")
 
+    implementation(platform("io.insert-koin:koin-bom:3.5.3"))
+    implementation("io.insert-koin:koin-android")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
